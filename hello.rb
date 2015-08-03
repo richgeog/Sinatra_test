4491d35852dfdd1ae63bce879e48d91e2233adb6
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-08-03 12:41:34 +0100

    image and secret page added

--- hello.rb
+++ hello.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require "shotgun"
 
 get '/' do
-  'hello'
+ "hello world
+  <div>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
+
+get '/secret' do
+  "This is a secret page!!!"
 end
